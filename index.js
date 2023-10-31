5ca527e65b20604e1cedf8226f4038b6651d324d
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-10-31 22:07:39 +0600

    added booking collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,10 +23,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        
+
         await client.connect();
 
         const serviceCollection = client.db("serviceDB").collection("services");
+        const bookingCollection = client.db("bookingDB").collection("bookings");
 
 
         app.get('/services', async (req, res) => {
@@ -35,11 +36,41 @@ async function run() {
 
             res.send(result);
         })
-        
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) };
+            const options = {
+                projection: { title: 1, img: 1, price: 1 },
+            };
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        // bookings
+        app.get('/bookings', async (req, res) => {
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        
+
         // await client.close();
     }
 }
